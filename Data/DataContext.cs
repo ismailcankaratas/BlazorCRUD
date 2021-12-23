42e7f3d3cbfa90a8293b0fb0eeabe2ff53f68995
ismail can <ismailcankaratasss@gmail.com>
2021-12-23 16:40:05 +0300

    Proje dosyası ekle.

---
+++ Data/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Post[] postsToSeed = new Post[10];
+            for (int i = 1; i < 11; i++ )
+            {
+                postsToSeed[i - 1] = new Post
+                {
+                    Id = i,
+                    Title = $"Post {i}",
+                    Content = $"This is some example cpntent for post {i}. The content is very interesting an I just wanted it to be two sentences long."
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
---
+++ Migrations/20211223085440_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorCRUD.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    Content = table.Column<string>(type: "TEXT", maxLength: 4096, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 1, "This is some example cpntent for post 1. The content is very interesting an I just wanted it to be two sentences long.", "Post 1" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 2, "This is some example cpntent for post 2. The content is very interesting an I just wanted it to be two sentences long.", "Post 2" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 3, "This is some example cpntent for post 3. The content is very interesting an I just wanted it to be two sentences long.", "Post 3" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 4, "This is some example cpntent for post 4. The content is very interesting an I just wanted it to be two sentences long.", "Post 4" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 5, "This is some example cpntent for post 5. The content is very interesting an I just wanted it to be two sentences long.", "Post 5" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 6, "This is some example cpntent for post 6. The content is very interesting an I just wanted it to be two sentences long.", "Post 6" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 7, "This is some example cpntent for post 7. The content is very interesting an I just wanted it to be two sentences long.", "Post 7" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 8, "This is some example cpntent for post 8. The content is very interesting an I just wanted it to be two sentences long.", "Post 8" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 9, "This is some example cpntent for post 9. The content is very interesting an I just wanted it to be two sentences long.", "Post 9" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[] { 10, "This is some example cpntent for post 10. The content is very interesting an I just wanted it to be two sentences long.", "Post 10" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
